09d9749eb62585d808326083fd5d99c4390b64a4
ReGYChang <p714140432@gmail.com>
2022-08-20 23:26:38 +0800

    neo4j: add findRoot function

---
+++ database/code/neo4j/match.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
+)
+
+func main() {
+	dbUri := "neo4j://localhost:7687"
+	driver, err := neo4j.NewDriver(dbUri, neo4j.BasicAuth("neo4j", "regy", ""))
+	if err != nil {
+		panic(err)
+	}
+
+	// Handle driver lifetime based on your application lifetime requirements  driver's lifetime is usually
+	// bound by the application lifetime, which usually implies one driver instance per application
+	defer driver.Close()
+
+	session := driver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	testSn := "2M1829A8NEFR"
+
+	if err := findRoot(session, testSn); err != nil {
+		panic(err)
+	}
+}
+
+func findRoot(session neo4j.Session, sn string) error {
+	params := map[string]interface{}{
+		"sn": sn,
+	}
+	_, err := session.Run(`
+		MATCH (n)
+		WHERE n.sn = $sn
+		WITH n, [(n) - [:BELONGS*] -> (x:MO) | x] AS ancestors
+		RETURN n, ancestors
+	`, params)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
